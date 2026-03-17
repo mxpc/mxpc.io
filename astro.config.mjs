6f084f9fef5fdf8329f0567858f08ab57194bb2f
olliethomas <1835492+olliethomas@users.noreply.github.com>
2026-03-17 16:07:53 +0000

    Hide docs section from public site for now

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
 
 export default defineConfig({
   site: 'https://mxpc.io',
-  integrations: [
-    starlight({
-      title: 'MX+C Docs',
-      social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/mxpc' }],
-      sidebar: [
-        {
-          label: 'Documentation',
-          autogenerate: { directory: 'docs' },
-        },
-      ],
-      customCss: ['./src/styles/starlight.css'],
-    }),
-  ],
 });
